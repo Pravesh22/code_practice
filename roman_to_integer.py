d06ae9ee32994b3a5c9ed3096d7d2c53ce7f75b5
Pravesh22 <budhathokipravesh5@gmail.com>
2022-01-08 13:13:46 +0545

    nothing changed

--- roman_to_integer.py
+++ roman_to_integer.py
@@ -1,23 +1,21 @@
 """
 -- Created by Pravesh Budhathoki
--- Treeleaf Technologies Pvt. Ltd.
--- Created on 2022-01-08 
+-- Created on 2022-01-08
 """
 
 """To convert roman integer to number"""
 
 def romanToInt(s):
     # Define the value of variable in dict format
-    roman_dict = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
-    n = len(s)
-    num = roman_dict[s[n-1]]
-
-    for i in range(n-2, -1, -1):
-        if roman_dict[s[i]] >= roman_dict[s[i+1]]:
-            num += roman_dict[s[i]]
-        else:
-            num -= roman_dict[s[i]]
-    return num
+    ans = 0
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+             'C': 100, 'D': 500, 'M': 1000}
+    for a, b in zip(s, s[1:]):
+      if roman[a] < roman[b]:
+        ans -= roman[a]
+      else:
+        ans += roman[a]
+    return ans + roman[s[-1]]
 
 if __name__ == '__main__':
-    print(romanToInt("IX"))
+    print(romanToInt("MCMXCIV"))
