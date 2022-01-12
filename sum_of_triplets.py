15d7841c58295fc4d028497323ec5c49a283cf7a
Pravesh22 <budhathokipravesh5@gmail.com>
2022-01-12 15:48:56 +0545

    3 sum problem of leetcode

--- sum_of_triplets.py
+++ sum_of_triplets.py
@@ -7,34 +7,6 @@
     nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0."""
 
 
-# def threeSum(nums):
-#     nums.sort()
-#     result = []
-#     n = len(nums)
-#     for i in range(n):
-#         if i > 0 and nums[i] == nums[i-1]:
-#             continue
-#         head = i + 1
-#         end = n - 1
-#         while head < end:
-#             if nums[i] + nums[head] + nums[end] == 0:
-#                 result.append([nums[i], nums[head], nums[end]])
-#                 head += 1
-#                 while head < end and nums[head] == nums[head-1]:
-#                     head += 1
-#                 end -= 1
-#                 while head < end and nums[end] == nums[end+1]:
-#                     end -= 1
-#             elif nums[i] + nums[head] + nums[end] < 0:
-#                 head += 1
-#                 while head < end and nums[head] == nums[head-1]:
-#                     head += 1
-#             else:
-#                 end -= 1
-#                 while head < end and nums[end] == nums[end+1]:
-#                     end -= 1
-#     return result
-
 def threeSum(nums):
     nums.sort()
     n = len(nums)
